869f5c24d3a862b734a13c8ab03cf90646ddb231
zhou quan wang <qwzhou89@icloud.com>
2020-10-15 17:31:06 +0800

    modify the install script to use modified repository.

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 echo "Copy scripts..."
 mkdir -p /etc/zabbix/scripts
-curl https://raw.githubusercontent.com/leshak/zabbix-mongodb/master/scripts/zabbix-mongodb-stats.sh -o /etc/zabbix/scripts/zabbix-mongodb-stats.sh
-curl https://raw.githubusercontent.com/leshak/zabbix-mongodb/master/scripts/zabbix-mongodb.py -o /etc/zabbix/scripts/zabbix-mongodb.py
+curl https://raw.githubusercontent.com/qwzhou89/zabbix-mongodb/master/scripts/zabbix-mongodb-stats.sh -o /etc/zabbix/scripts/zabbix-mongodb-stats.sh
+curl https://raw.githubusercontent.com/qwzhou89/zabbix-mongodb/master/scripts/zabbix-mongodb.py -o /etc/zabbix/scripts/zabbix-mongodb.py
 chmod 0740 /etc/zabbix/scripts/*
 chown -R zabbix. /etc/zabbix/scripts
 
 echo "Copy config..."
-curl https://raw.githubusercontent.com/leshak/zabbix-mongodb/master/zabbix_agentd.d/mongodb.conf -o /etc/zabbix/zabbix_agentd.d/mongodb.conf
+curl https://raw.githubusercontent.com/qwzhou89/zabbix-mongodb/master/zabbix_agentd.d/mongodb.conf -o /etc/zabbix/zabbix_agentd.d/mongodb.conf
 
 echo "Restart zabbix agent..."
 systemctl restart zabbix-agent
